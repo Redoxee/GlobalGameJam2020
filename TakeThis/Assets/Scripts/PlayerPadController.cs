e9bb4d632e88b11663fd221e352d478fa56fb24f
Redoxee <antonmakesgames@gmail.com>
2020-02-01 16:51:56 +0100

    Tweaking inputs

---
+++ TakeThis/Assets/Scripts/PlayerPadController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPadController : MonoBehaviour
+{
+    public Rigidbody2D PlayerRigidBody = null;
+    public float Speed;
+    public void LateUpdate()
+    {
+        float ix = Input.GetAxis("Horizontal");
+        float iy = Input.GetAxis("Vertical");
+
+        this.PlayerRigidBody.velocity += new Vector2(ix, iy) * Speed;
+    }
+}
