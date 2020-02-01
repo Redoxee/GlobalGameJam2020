c6698c112fda99bf14939e20f47b6c2dfc8848ec
Redoxee <antonmakesgames@gmail.com>
2020-02-01 01:08:52 +0100

    Testing stuff

---
+++ TakeThis/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody2D PlayerRigidBody = null;
+
+    public float Speed = 2.0f;
+
+    private void LateUpdate()
+    {
+        Vector2 direction = Vector2.zero;
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            direction.x -= 1;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            direction.x += 1;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            direction.y -= 1;
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            direction.y += 1;
+        }
+
+        if (direction.sqrMagnitude > 0)
+        {
+            this.PlayerRigidBody.velocity = direction * this.Speed;
+        }
+    }
+}
