be88ee4e9b9d8105a8eae9cab9bc3d013fd200e0
Redoxee <antonmakesgames@gmail.com>
2020-02-02 11:19:33 +0100

    Binded the screens

---
+++ TakeThis/Assets/Scripts/PressAToPlay.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressAToPlay : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetButtonDown("PatPat") || Input.GetKeyDown(KeyCode.A))
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene("GameScene");
+        }
+    }
+}
