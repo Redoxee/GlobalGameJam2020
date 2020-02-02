fec24d203360019b6ffc61b72af1ede32df23da8
Germain_Daniel <germaindaniel.lefebvre@gmail.com>
2020-02-02 02:25:40 +0100

    Setup de la scene du menu + texte blinking et connexion à la scene in-game

---
+++ TakeThis/Assets/Scripts/Menu.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour {
+
+    public Text text;
+    public bool isBlinking = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("BlinkingText");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            SceneManager.LoadScene(1);
+        }
+    }
+
+    public IEnumerator BlinkingText() 
+    {
+    while (isBlinking)
+    {
+        text.enabled = true;
+        yield return new WaitForSeconds(1);
+        text.enabled = false;
+        yield return new WaitForSeconds(.5f);
+    }
+    }
+}
